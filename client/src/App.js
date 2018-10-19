706c8189e7d775dd14fb825be21cce2bb2aeb0d1
Carlos Vargas <cornielleandres@gmail.com>
2018-10-19 11:29:41 -0400

    Create front-end client.

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// Components
+import { Header } from './components/index.js';
+
+class App extends Component {
+	render() {
+		return (
+			<div className = 'App'>
+				<Header />
+			</div>
+		);
+	}
+}
+
+export default App;
