346e8de4bf2dcff6bbd80fcddb662a9bfaaad98e
Carlos Vargas <cornielleandres@gmail.com>
2018-10-19 11:33:07 -0400

    Add helmet middleware to back-end server.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+const express   = require('express');
+const cors      = require('cors');
+const helmet    = require('helmet');
 
 const configureRoutes = require('./config/routes');
 
@@ -12,6 +13,7 @@ const corsOptions = {
 
 server.use(express.json());
 server.use(cors());
+server.use(helmet());
 
 configureRoutes(server);
 
