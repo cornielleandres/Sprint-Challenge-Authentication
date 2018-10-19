c928cd71e9f454632d9d8fe87715a8f5d96121c9
Carlos Vargas <cornielleandres@gmail.com>
2018-10-19 10:44:09 -0400

    Implement register function in routes.

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
-const axios = require('axios');
+const axios             = require('axios');
+const userDb            = require('../database/models/userDb.js');
+const bcrypt            = require('bcryptjs');
+const jwt               = require('jsonwebtoken');
+const { jwtKey }        = require('../_secrets/keys.js');
 
-const { authenticate } = require('./middlewares');
+const { authenticate }  = require('./middlewares');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -8,8 +12,38 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+const secret = jwtKey;
+const generateNewToken = user => {
+  const payload = user;
+  const options = {
+    expiresIn: '10m', // 10 minutes
+  };
+  return jwt.sign(payload, secret, options);
+};
+
 function register(req, res) {
-  // implement user registration
+  const credentials = req.body;
+  return bcrypt
+    .hash(credentials.password, 12, function(bcryptErr, hash) {
+      if (bcryptErr) {
+        return res.status(500).json({ error: `Bcrypt hashing failed: ${ bcryptErr }` });
+      }
+      credentials.password = hash;
+      return userDb
+        .register(credentials)
+        .then(id => {
+          const user = {
+            id: id,
+            username: credentials.username,
+          };
+          const token = generateNewToken(user);
+          return res.status(201).json({
+            newUser: credentials.username,
+            token: token,
+          });
+        })
+        .catch(err => res.status(500).json({ error: `Server failed to register new user: ${ err }` }));
+    });
 }
 
 function login(req, res) {
